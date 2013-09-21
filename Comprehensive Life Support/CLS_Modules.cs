32582a0011ab3019f81fb667deb0f070a93b4927
Tahvohck <Tahvohck@gmail.com>
2013-09-21 13:37:08 -0400

    PartModule creation: CLS_LivableArea

---
+++ Comprehensive Life Support/CLS_Modules.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+class CLS_LivableArea : PartModule
+{
+	[KSPEvent(active=true, guiActive=true,guiName="Hide CLS")]
+	private void hideGUI() {
+		Events["showGUI"].active = true;
+		Events["hideGUI"].active = false;
+		CLS_FlightGui.hideStatusWindow();
+	}
+	[KSPEvent(active = true, guiActive = true, guiName = "Show CLS")]
+	private void showGUI() {
+		Events["showGUI"].active = false;
+		Events["hideGUI"].active = true;
+		CLS_FlightGui.showStatusWindow();
+	}
+
+	private static void Awake() {
+		CDebug.log("LivableArea Awoken.");
+	}
+	private static void Start() {
+		CDebug.log("LivableArea started.");
+	}
+
+	public override void OnLoad(ConfigNode node) {
+		base.OnLoad(node);
+		CDebug.log(node.ToString());
+	}
+}
