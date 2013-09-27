a6dbdb649bf4b4bdbb305a529127ee526e1eebe6
Tahvohck <Tahvohck@gmail.com>
2013-09-26 23:08:40 -0400

    Hid Events
    These really shouldn't be on a per-part basis. Fix it later.

--- Comprehensive Life Support/CLS_Modules.cs
+++ Comprehensive Life Support/CLS_Modules.cs
@@ -8,18 +8,18 @@ using UnityEngine;
 
 class CLS_LivableArea : PartModule
 {
-	[KSPEvent(active=true, guiActive=true,guiName="Hide CLS")]
-	private void hideGUI() {
-		Events["showGUI"].active = true;
-		Events["hideGUI"].active = false;
-		CLS_FlightGui.hideStatusWindow();
-	}
-	[KSPEvent(active = true, guiActive = true, guiName = "Show CLS")]
-	private void showGUI() {
-		Events["showGUI"].active = false;
-		Events["hideGUI"].active = true;
-		CLS_FlightGui.showStatusWindow();
-	}
+	//[KSPEvent(active=true, guiActive=true,guiName="Hide CLS")]
+	//private void hideGUI() {
+	//	Events["showGUI"].active = true;
+	//	Events["hideGUI"].active = false;
+	//	CLS_FlightGui.hideStatusWindow();
+	//}
+	//[KSPEvent(active = true, guiActive = true, guiName = "Show CLS")]
+	//private void showGUI() {
+	//	Events["showGUI"].active = false;
+	//	Events["hideGUI"].active = true;
+	//	CLS_FlightGui.showStatusWindow();
+	//}
 
 	private static void Awake() {
 		CDebug.log("LivableArea Awoken.");
