29b68d34ae67abf31b214915ca7e4109cf9e8a27
Tahvohck <Tahvohck@gmail.com>
2013-10-20 14:32:48 -0400

    LivableArea is now in Beta-Semicomplete
    Beta-SemiComplete:
    Part is in a functional, but pending review for "completeness"

--- Comprehensive Life Support/CLS_Modules.cs
+++ Comprehensive Life Support/CLS_Modules.cs
@@ -23,19 +23,20 @@ class CLS_LivableArea : PartModule
 
 	private static void Awake() { CDebug.log("LivableArea Awoken."); }
 	private static void Start() { CDebug.log("LivableArea started."); }
-
-
 	public override void OnStart(StartState state) {
 		if (HighLogic.LoadedSceneIsFlight)
 			foreach (ProtoCrewMember crewMem in part.protoModuleCrew)
 				if (!Backend.KerbalHealth.ContainsKey(crewMem.name))
 					Backend.KerbalHealth[crewMem.name] = new KerbalBiometric();
 	}
-
-
 	public override void OnLoad(ConfigNode node) { CDebug.log(node.ToString()); }
 
 
+	/// <summary>
+	/// Fixed Update:
+	/// Do resource consumption/creation - O2, CO2, food, water
+	/// Do break chance tick [x]
+	/// </summary>
 	public void FixedUpdate() {
 		int secondsPerDayFactor = 86400;
 		double o2Want, snacksWant, waterWant;
@@ -54,11 +55,11 @@ class CLS_LivableArea : PartModule
 			foreach (ProtoCrewMember crewMem in part.protoModuleCrew)
 				if (Backend.KerbalHealth[crewMem.name].bloodstreamOxygen > 0) {
 					Backend.KerbalHealth[crewMem.name].bloodstreamOxygen -= (dTime * percentOfWanted);
-					CDebug.log(crewMem.name + " has " + Math.Floor(Backend.KerbalHealth[crewMem.name].bloodstreamOxygen) + " seconds to live.");
+					//CDebug.log(crewMem.name + " has " + Math.Floor(Backend.KerbalHealth[crewMem.name].bloodstreamOxygen) + " seconds to live.");
 				}
 				else {
 					crewMem.Die();
-					crewMem.seat.DespawnCrew();
+					part.RemoveCrewmember(crewMem);
 				}
 		}
 		else
@@ -69,6 +70,12 @@ class CLS_LivableArea : PartModule
 		co2Made = part.RequestResource("CO2", crewSize * dTime * ConfigSettings.ratesPerKerbal["CO2"] * (o2Got / o2Want) /
 			(secondsPerDayFactor / ConfigSettings.timeScale));
 		//(o2Got / o2Want) is to ensure that we only produce an equivalent amount of CO2 to the O2 we consumed.
+		//TODO CO2 KILL
+		if(part.Resources["CO2"].amount == part.Resources["CO2"].maxAmount)
+			foreach (ProtoCrewMember crewMem in part.protoModuleCrew) {
+				crewMem.Die();
+				part.RemoveCrewmember(crewMem);
+			}
 
 		//get food
 		//TODO I want this to eventually be a periodic thing instead of a constant.
@@ -82,16 +89,38 @@ class CLS_LivableArea : PartModule
 			foreach (ProtoCrewMember crewMem in part.protoModuleCrew)
 				if (Backend.KerbalHealth[crewMem.name].bloodstreamSnacks > 0)
 					Backend.KerbalHealth[crewMem.name].bloodstreamSnacks -= (dTime * percentOfWanted);
-				else
+				else {
 					crewMem.Die();
+					part.RemoveCrewmember(crewMem);
+				}
 		}
 		else
 			foreach (ProtoCrewMember crewMem in part.protoModuleCrew)
 				Backend.KerbalHealth[crewMem.name].resetSnacks();
 
+
 		//get water
+		waterWant = crewSize * dTime * ConfigSettings.ratesPerKerbal["Water"] /
+			(secondsPerDayFactor / ConfigSettings.timeScale);
 		waterGot = part.RequestResource("Water", crewSize * dTime * ConfigSettings.ratesPerKerbal["Water"] /
 			(secondsPerDayFactor / ConfigSettings.timeScale));
+		if (waterGot == 0) {
+			double amountLeft = Backend.getCurrentAmount("Water");
+			double percentOfWanted = (waterWant - amountLeft) / waterWant;
+			waterGot = part.RequestResource("Water", amountLeft);
+			foreach (ProtoCrewMember crewMem in part.protoModuleCrew)
+				if (Backend.KerbalHealth[crewMem.name].bloodstreamWater > 0)
+					Backend.KerbalHealth[crewMem.name].bloodstreamWater -= (dTime * percentOfWanted);
+				else {
+					crewMem.Die();
+					part.RemoveCrewmember(crewMem);
+				}
+		}
+		else
+			foreach (ProtoCrewMember crewMem in part.protoModuleCrew)
+				Backend.KerbalHealth[crewMem.name].resetWater();
 
+
+		//Roll for breaks
 	}
 }
